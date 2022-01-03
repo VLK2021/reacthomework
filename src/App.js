c188968635ee556a4ab81e2d8e0cffc38b0778a1
VLK2021 <volodymyrkostiuk2021@gmail.com>
2022-01-03 21:14:51 +0200

    only component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Car from "./components/car/car";
 
 function App() {
 
     return (
         <div>
+            <div>
+                <Car model={'Honda'} year={"2006"} power={"1.8"}/>
+            </div>
 
+            <div>
+                <Car model={"Toyota"} year={"2010"} power={"2.5"}/>
+            </div>
         </div>
     );
 }
+
 export default App;
---
+++ src/components/car/car.js
@@ -0,0 +1,11 @@
+export default function Car(props) {
+    let {model, year, power} = props;
+    return(
+        <h1>
+            {model} - {year} - {power}
+        </h1>
+    )
+}
+
+
+
