7f1cce46cb74c45b46c7bb45a00e04f40a2bff11
VLK2021 <volodymyrkostiuk2021@gmail.com>
2022-01-03 18:51:14 +0200

    forms noControl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+
+    let [formState, setFormState]= useState({login: '', pasword: ``});
+
+    function onSubmit(e) {
+        console.log(e.target.login.value);
+        console.log(e.target.pasword.value);
+        e.preventDefault()
+        console.log(formState);
+
+    }
+    
+    function onChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+
   return (
     <div>
+
+      <form onSubmit={onSubmit}>
+        <input type="text" name={'login'} value={formState.login} onChange={onChange}/>
+        <input type="text" name={'pasword'} value={formState.pasword} onChange={onChange}/>
+        <button>click me</button>
+      </form>
 
     </div>
   );
