d7d08161807617827abe75a1315ee236eff1a9e2
VLK2021 <volodymyrkostiuk2021@gmail.com>
2022-01-03 21:04:44 +0200

    masters general

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import Car from "./component/component";
 
 function App() {
 
-
-
     return (
         <div>
-            <div>
-                <Car model ={'Honda'} year ={"2006"} power={"1.8"}/>
-            </div>
 
-            <div>
-                <Car model ={"Toyota"} year ={"2010"} power={"2.5"}/>
-            </div>
         </div>
     );
 }
-
 export default App;
