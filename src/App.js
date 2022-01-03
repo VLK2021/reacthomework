82bf1d5b2bfc6f15a34ab24eb70e0f4930c4e832
VLK2021 <volodymyrkostiuk2021@gmail.com>
2022-01-03 19:20:56 +0200

    refForms createRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
 import './App.css';
+import {createRef} from "react";
+
 
 function App() {
+
+  let login = createRef();
+  let password = createRef();
+
+  function onSubmit(e) {
+    console.log(login.current.value);
+    console.log(password.current.value);
+    e.preventDefault();
+  }
+
   return (
     <div>
-
+      <form onSubmit={onSubmit}>
+        <input type="text" name={'login'} ref={login} placeholder={'login'}/>
+        <input type="text" name={'password'} ref={password} placeholder={'password'}/>
+        <button>click me</button>
+      </form>
     </div>
   );
 }
