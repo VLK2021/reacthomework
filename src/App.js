15bdf6064c48652a73d725b498287681215bf943
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-13 22:26:14 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
+//варіант номер 1
+    const [counter, setCounter] = useState([0, 0, 0]);
 
     return (
         <div>
+            <div>
+                <button onClick={() => setCounter([counter[0] + 1, counter[1], counter[2]])}>Тиць+:</button>
+                {counter[0]}
+                <button onClick={() => setCounter([counter[0] - 1, counter[1], counter[2]])}>Тиць-:</button>
+            </div>
 
+            <div>
+                <button onClick={() => setCounter([counter[0], counter[1] + 1, counter[2]])}>Тиць+:</button>
+                {counter[1]}
+                <button onClick={() => setCounter([counter[0], counter[1] - 1, counter[2]])}>Тиць-:</button>
+            </div>
+
+            <div>
+                <button onClick={() => setCounter([counter[0], counter[1], counter[2] + 1])}>Тиць+:</button>
+                {counter[2]}
+                <button onClick={() => setCounter([counter[0], counter[1], counter[2] - 1])}>Тиць-:</button>
+            </div>
         </div>
     );
+
+    //варіант номер 2
+
+    // const [] = useState([]);
+    // return (
+    //     <div>
+    //
+    //     </div>
+    // );
 }
 
 export default App;
