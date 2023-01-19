76b275a27eff4019662b67cb95f6211b17cd4d1c
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-01-19 15:41:21 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {useState} from "react";
+
 import './App.css';
 import Cars from "./components/Cars/Cars";
 import Form from "./components/Form/Form";
-import {useState} from "react";
+
 
 function App() {
     const [trigger, setTrigger] = useState(null);
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+
 import {carService} from "../../services/car.service";
+
 
 const Form = ({update}) => {
 
